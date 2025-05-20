19335b61ba62f1b79681adc24ae56006ef1cea25
Julian Montufar <jamontuf@bancolombia.com.co>
2025-05-20 14:46:25 -0500

    refactor list menu dashboard

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,7 +1,32 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
+interface MenuOptions {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 @Component({
   selector: 'gifs-side-menu-options',
   templateUrl: './side-menu-options.component.html',
+  imports: [RouterLink, RouterLinkActive]
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    }
+  ]
+
+}
